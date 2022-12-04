9ea52c87156a88d91222ffe7bdc59333d64e35aa
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2022-12-03 19:08:04 -0500

    bug fixes

--- index.js
+++ index.js
@@ -1,56 +1,46 @@
 const fs = require("fs");
-const path = require("path");
 
-export default class JDB {
-    dbName = "jdb/";
-    create = (input, name = "") => {
-        if (!fs.existsSync(this.dbName + name)) {
-            fs.mkdirSync(this.dbName + name, { recursive: true });
+class JDB {
+    create = (input, location) => {
+        if (!fs.existsSync(location)) {
+            fs.mkdirSync(location, { recursive: true });
         }
         fs.writeFileSync(
-            path.join(__dirname, this.dbName + name + "/jdb.json"),
+            location + "/jdb.json",
             JSON.stringify(input, null, 4)
         );
     };
-    read = (name = "") => {
-        if (fs.existsSync(this.dbName + name)) {
-            return JSON.parse(
-                fs.readFileSync(
-                    path.join(__dirname, this.dbName + name + "/jdb.json")
-                )
-            );
+    read = (location) => {
+        if (fs.existsSync(location)) {
+            return JSON.parse(fs.readFileSync(location + "/jdb.json"));
         } else {
             return "Database not found";
         }
     };
-    update = (input, name = "") => {
-        if (fs.existsSync(this.dbName + name)) {
+    update = (input, location) => {
+        if (fs.existsSync(location)) {
             fs.writeFileSync(
-                path.join(__dirname, this.dbName + name + "/jdb.json"),
+                location + "/jdb.json",
                 JSON.stringify(input, null, 4)
             );
         } else {
             return "Database not found";
         }
     };
-    delete = (name = "") => {
-        if (fs.existsSync(this.dbName + name)) {
-            fs.rmSync(this.dbName + name, { recursive: true });
+    delete = (location) => {
+        if (fs.existsSync(location)) {
+            fs.rmSync(location, { recursive: true });
         } else {
             return "Database not found";
         }
     };
-    print = (name = "") => {
-        if (fs.existsSync(this.dbName + name)) {
-            console.log(
-                JSON.parse(
-                    fs.readFileSync(
-                        path.join(__dirname, this.dbName + name + "/jdb.json")
-                    )
-                )
-            );
+    print = (location) => {
+        if (fs.existsSync(location)) {
+            console.log(JSON.parse(fs.readFileSync(location + "/jdb.json")));
         } else {
             return "Database not found";
         }
     };
 }
+
+module.exports = JDB;
