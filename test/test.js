57a8124b5b5d25d27014be47a991649bf73eae5d
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-05-13 01:31:31 -0400

    add tests

---
+++ test/test.js
@@ -0,0 +1,69 @@
+import * as path from "path";
+import JDB from "jsoneng";
+
+const dirname = path.join(process.cwd());
+const jdb = new JDB(path.join(dirname, "jdb"));
+
+function runTests() {
+    // Helper function to check if two objects are deeply equal
+    function deepEqual(actual, expected) {
+        return JSON.stringify(actual) === JSON.stringify(expected);
+    }
+
+    async function testCreate() {
+        await jdb.create({ key1: "value1" }, "db1");
+        const data = await jdb.read("db1");
+        if (deepEqual(data, { key1: "value1" })) {
+            console.log("create: Passed");
+        } else {
+            console.error("create: Failed");
+        }
+    }
+
+    async function testRead() {
+        const data = await jdb.read("db1");
+        if (deepEqual(data, { key1: "value1" })) {
+            console.log("read: Passed");
+        } else {
+            console.error("read: Failed");
+        }
+    }
+
+    async function testUpdate() {
+        await jdb.update({ key1: "updatedValue" }, "db1");
+        const data = await jdb.read("db1");
+        if (deepEqual(data, { key1: "updatedValue" })) {
+            console.log("update: Passed");
+        } else {
+            console.error("update: Failed");
+        }
+    }
+
+    async function testDelete() {
+        await jdb.delete("db1");
+        try {
+            await jdb.read("db1");
+            console.error("delete: Failed");
+        } catch (error) {
+            if (error.message === "Database not found") {
+                console.log("delete: Passed");
+            } else {
+                console.error("delete: Failed");
+            }
+        }
+    }
+
+    async function runAllTests() {
+        try {
+            await testCreate();
+            await testRead();
+            await testUpdate();
+            await testDelete();
+        } catch (error) {
+            console.error("Test failed with error:", error.message);
+        }
+    }
+    runAllTests();
+}
+
+runTests();
